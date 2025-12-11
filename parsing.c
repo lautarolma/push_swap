76732f385023ac17ca5fa9e5c3b06755992e4fbe
laviles <laviles@student.42madrid.com>
2025-12-11 23:40:52 +0100

    steping no where

--- parsing.c
+++ parsing.c
@@ -1,3 +1,44 @@
 #include "push_swap.h"
 
+char	*get_next_word(char *argv[i], token_counter);
+{
+	
+}
+
 t_list	stack_creator(char *argv)
+{
+	static char	*token_counter;
+	char		**head;
+	t_list		node;
+	int			i;
+
+	token_counter = 0;
+	while(argv[i][0])
+	node.content = get_next_word(argv[i], token_counter);
+	node.index = get_index(node.content, token_counter)
+	if (!node.content)
+		return (0);
+	node.next = NULL;
+	i++;
+}
+
+int	main()
+{
+
+
+	while (argv[i])
+	{
+		while (argv[i][j])
+		{
+			if (argv[i][j] == ' ')
+			{
+				while (node)
+					token = node_creator(ft_strtok(argv[i]));
+			}
+			else
+			
+			j++;
+		}
+	}
+}
+
